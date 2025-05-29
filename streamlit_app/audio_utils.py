29f8bfc3c7068b8a1b801d03663d3b6f083410af
Sri krishna vamsi <100073469+srikrishnavansi@users.noreply.github.com>
2025-05-29 18:16:10 +0530

    Update audio_utils.py

--- streamlit_app/audio_utils.py
+++ streamlit_app/audio_utils.py
@@ -1,9 +1,2 @@
-from pydub import AudioSegment
-import io
-
 def ensure_wav_format(audio_bytes):
-    audio = AudioSegment.from_file(io.BytesIO(audio_bytes))
-    audio = audio.set_frame_rate(16000).set_channels(1).set_sample_width(2)
-    out_io = io.BytesIO()
-    audio.export(out_io, format="wav")
-    return out_io.getvalue()
+    return audio_bytes
